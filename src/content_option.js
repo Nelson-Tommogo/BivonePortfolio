e1d62da33682d0f0a4ac5d7a0e97425bb6514909
SmartMavuno <info@smartmavuno.com>
2025-01-17 17:20:01 +0300

    No message

---
+++ src/content_option.js
@@ -0,0 +1,135 @@
+const logotext = "Bivone Rapolo";
+const meta = {
+    title: "Bivone Rapolo",
+    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Bivon Rapolo",
+    animated: {
+        first: "I love Marketing",
+        second: "I Love Project management",
+        third: "I love Creating slides/presentations, meeting agendas, travel itineraries",
+    },
+    description: "Hi, I’m Bivone Rapolo, a dedicated virtual assistant passionate about helping individuals and businesses thrive. With expertise in email marketing and copywriting, I craft messages that captivate and convert, ensuring your brand resonates with your audience. By working with me, you’ll gain a reliable partner committed to your success, giving you more time to focus on growing your business and achieving your goals. Let’s turn your workload into opportunities for growth and efficiency.",
+    your_img_url: "../src/assets/images/react-porfolio.png",
+};
+
+
+const dataabout = {
+    title: "About Me",
+    aboutme: "Hi, I’m Bivon Rapolo, a dedicated virtual assistant passionate about helping individuals and businesses thrive. I specialize in email marketing and project management, ensuring that your brand's voice resonates with your audience.",
+};
+const worktimeline = [
+    {
+        jobtitle: "Email Marketing Specialist",
+        where: "SmartMavuno Limited",
+        date: "2023 - Present",
+    },
+    {
+        jobtitle: "Virtual Assistant",
+        where: "Freelance",
+        date: "2021 - 2022",
+    },
+    {
+        jobtitle: "Project Coordinator",
+        where: "KilonzoCorp",
+        date: "2020 - 2021",
+    },
+];
+
+const skills = [
+    {
+        name: "Email Marketing",
+        value: 90,
+    },
+    {
+        name: "Project Management",
+        value: 85,
+    },
+    {
+        name: "Copywriting",
+        value: 80,
+    },
+    {
+        name: "Google Suite",
+        value: 75,
+    },
+    {
+        name: "Microsoft Office",
+        value: 80,
+    },
+    {
+        name: "Scheduling",
+        value: 90,
+    },
+];
+
+const services = [
+    {
+        title: "Email Marketing",
+        description: "Crafting captivating emails that engage and convert your audience.",
+    },
+    {
+        title: "Project Management",
+        description: "Ensuring smooth project execution from planning to completion.",
+    },
+    {
+        title: "Presentation Design",
+        description: "Creating impactful slides and meeting agendas to enhance your communication.",
+    },
+];
+
+const dataportfolio = [
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Developed and executed an email marketing campaign for SmartMavuno that increased customer engagement by 30%.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "Crafted compelling email copy for Mebiut, resulting in improved open and click-through rates.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Managed multiple projects at Kilonzocorp simultaneously, ensuring timely delivery and coordination among team members.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "Performed general executive assistant duties for various clients, including scheduling, organizing meetings, and managing communications.",
+        link: "#",
+    },
+];
+
+
+const contactConfig = {
+    YOUR_EMAIL: "rapolobivone124@gmail.com",
+    YOUR_FONE: "+254 759 658692",
+    description: "I am dedicated to providing support and ensuring your success. Feel free to reach out for assistance with email marketing, project management, or executive support.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    instagram: "https://www.instagram.com/bivonerapollo?igsh=cHF6MTA2eTV5emM3",
+    facebook: "https://www.facebook.com/bivone.apollo",
+    linkedin: "https://linkedin.com",
+    twitter: "https://x.com/BivoneR?t=Rb2LZNgDJOWcsxHICLJ9yw&s=08",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
